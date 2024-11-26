6c3ad43d11040591182a2d30566296f4178c8991
saurabh <saurabhshindework@gmail.com>
2024-11-27 00:10:15 +0530

    make the changes in the name header...

--- src/Components/Header_Main.js
+++ src/Components/Header_Main.js
@@ -49,7 +49,7 @@ const Header_Main = ({ toggleSidebar }) => {
                 component="div"
                 sx={{ fontWeight: "bold", color: "white", marginLeft:{xs:"2px", md:"10px"}, fontSize:{xs:"100%", md:"150%"} }}
               >
-                <span style={{ color: "#F78D6A" }}>Expense</span>Wise
+                <span style={{ color: "#F78D6A" }}>Expense</span><span style={{ color: "white" }}>Wise</span>
               </Typography>
             </Box>
 
